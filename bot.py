fa8e9de04531bca241c01f1ade72ddc2bd525610
homik1999-ux <homik1999@gmail.com>
2026-04-03 09:43:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
-import requests
 
+print("START SKRYPTU")
+
+import requests
 TOKEN = "8745828890:AAFx0jw6fNFTLmuVZLD211oq0nZ8bbTrQw0"
 CHAT_ID = "8664071476"
 
+print("Wysyłam wiadomość...")
 
-def send_telegram(msg):
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    data = {
-        "chat_id": CHAT_ID,
-        "text": msg
-    }
-    requests.post(url, data=data)
+requests.post(
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    data={"chat_id": CHAT_ID, "text": "Test z GitHub Actions ✅"}
+)
 
-
-send_telegram("Bot działa poprawnie ✅ GitHub wysłał wiadomość.")
+print("KONIEC SKRYPTU")
