865ce447940cdafcdf21926433d8d55d513d08a1
homik1999-ux <homik1999@gmail.com>
2026-04-03 09:52:15 +0200

    Change Telegram bot token
    Updated the bot token for Telegram API.

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 
 print("START")
 
-TOKEN = "8745828890:AAFx0jw6fNFTLmuVZLD211oq0nZ8bbTrQw0"
+TOKEN = "8745828890:AAH1yLOCo4Yksy6a9mFhpGxUt6LZQoEohIk"
 CHAT_ID = "8664071476"
 
 response = requests.post(
