aea5d6aa5898682579d72b6e791beb3d475bfd61
homik1999-ux <homik1999@gmail.com>
2026-04-03 09:12:18 +0200

    Initialize bot with keywords and groups

---
+++ bot.py
@@ -0,0 +1,14 @@
+import time
+
+KEYWORDS = ["klimatyzacja", "montaż klimy", "klimatyzator"]
+
+GROUPS = [
+    "https://facebook.com/groups/TWOJA_GRUPA"
+]
+
+print("Bot uruchomiony")
+
+for group in GROUPS:
+    print("Sprawdzam:", group)
+
+print("Gotowe")
