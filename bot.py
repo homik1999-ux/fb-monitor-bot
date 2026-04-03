262b18b695ec27c2f54a82298f121b8d55592518
homik1999-ux <homik1999@gmail.com>
2026-04-03 09:41:47 +0200

    Remove keywords and groups, update startup message

--- bot.py
+++ bot.py
@@ -3,17 +3,6 @@ import requests
 TOKEN = "8745828890:AAFx0jw6fNFTLmuVZLD211oq0nZ8bbTrQw0"
 CHAT_ID = "8664071476"
 
-KEYWORDS = [
-    "klimatyzacja",
-    "montaż klimy",
-    "klimatyzator",
-    "pompa ciepła"
-]
-
-GROUPS = [
-    "https://www.facebook.com/groups/983446936989580"
-]
-
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
@@ -24,4 +13,4 @@ def send_telegram(msg):
     requests.post(url, data=data)
 
 
-send_telegram("Bot działa ✅ Sprawdzanie grup uruchomione.")
+send_telegram("Bot działa poprawnie ✅ GitHub wysłał wiadomość.")
